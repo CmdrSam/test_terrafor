884ce8a6e5d2e0b07cfc67a522de81d2a129c980
Saumya Verma <42969251+CmdrSam@users.noreply.github.com>
2025-02-08 17:06:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,19 @@
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.my_cluster.id
+output "id" {
+  value = aws_ecs_cluster.example.id
 }
 
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.my_cluster.name
+output "name" {
+  value = aws_ecs_cluster.example.name
 }
 
-output "s3_bucket_id" {
+output "id" {
   value = aws_s3_bucket.my_bucket.id
 }
 
-output "s3_bucket_arn" {
+output "arn" {
   value = aws_s3_bucket.my_bucket.arn
 }
 
-output "s3_bucket_domain_name" {
-  value = aws_s3_bucket.my_bucket.bucket_domain_name
-}
-
-output "s3_bucket_regional_domain_name" {
-  value = aws_s3_bucket.my_bucket.bucket_regional_domain_name
-}
-
-output "s3_bucket_website_endpoint" {
-  value = aws_s3_bucket.my_bucket.website_endpoint
+output "region" {
+  value = aws_s3_bucket.my_bucket.region
 }
